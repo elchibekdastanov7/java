7f288247a13e320a0e474f943dc5a0ac3b213852
elchibekdastanov7 <elchibekdasatnov7@gmail.com>
2023-01-10 20:46:47 +0600

    commint2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Bye world!!!");
-        }
     }
 }
